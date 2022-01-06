eab57d28eda169c93b952f42e158b1f054d6b6ba
Kir_Antipov <kp.antipov@gmail.com>
2022-01-06 13:43:46 +0300

    Depercated accesswidener in favor of built-in fabric api stuff

--- src/main/java/dev/kir/netherchest/block/entity/NetherChestBlockEntities.java
+++ src/main/java/dev/kir/netherchest/block/entity/NetherChestBlockEntities.java
@@ -2,27 +2,20 @@ package dev.kir.netherchest.block.entity;
 
 import dev.kir.netherchest.NetherChest;
 import dev.kir.netherchest.block.NetherChestBlocks;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 
-import java.util.function.BiFunction;
-
 public final class NetherChestBlockEntities {
-    public static final BlockEntityType<NetherChestBlockEntity> NETHER_CHEST;
-
-    static {
-        NETHER_CHEST = register("nether_chest", NetherChestBlockEntity::new, NetherChestBlocks.NETHER_CHEST);
-    }
+    public static final BlockEntityType<NetherChestBlockEntity> NETHER_CHEST = register("nether_chest", NetherChestBlockEntity::new, NetherChestBlocks.NETHER_CHEST);
 
     public static void init() { }
 
-    private static <T extends BlockEntity> BlockEntityType<T> register(String id, BiFunction<BlockPos, BlockState, T> supplier, Block block) {
+    private static <T extends BlockEntity> BlockEntityType<T> register(String id, FabricBlockEntityTypeBuilder.Factory<T> factory, Block block) {
         Identifier trueId = NetherChest.locate(id);
-        return Registry.register(Registry.BLOCK_ENTITY_TYPE, trueId, BlockEntityType.Builder.create(supplier::apply, block).build(null));
+        return Registry.register(Registry.BLOCK_ENTITY_TYPE, trueId, FabricBlockEntityTypeBuilder.create(factory, block).build());
     }
 }
--- src/main/java/dev/kir/netherchest/client/render/NetherChestRenderers.java
+++ src/main/java/dev/kir/netherchest/client/render/NetherChestRenderers.java
@@ -6,7 +6,7 @@ import dev.kir.netherchest.block.entity.NetherChestBlockEntities;
 import dev.kir.netherchest.item.NetherChestItems;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
@@ -15,7 +15,7 @@ import net.minecraft.util.math.BlockPos;
 @Environment(EnvType.CLIENT)
 public final class NetherChestRenderers {
     static {
-        BlockEntityRendererRegistry.INSTANCE.register(NetherChestBlockEntities.NETHER_CHEST, NetherChestBlockEntityRenderer::new);
+        BlockEntityRendererRegistry.register(NetherChestBlockEntities.NETHER_CHEST, NetherChestBlockEntityRenderer::new);
 
         BlockEntity renderEntity = NetherChestBlockEntities.NETHER_CHEST.instantiate(BlockPos.ORIGIN, NetherChestBlocks.NETHER_CHEST.getDefaultState());
         BuiltinItemRendererRegistry.INSTANCE.register(NetherChestItems.NETHER_CHEST, (itemStack, transform, stack, source, light, overlay) ->
